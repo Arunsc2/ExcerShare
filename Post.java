407e2889b9a55372824eea44203cec431ccc3ffc
Arunsc2 <70555936+Arunsc2@users.noreply.github.com>
2021-02-07 01:30:18 -0500

    Update Post.java

--- Post.java
+++ Post.java
@@ -5,21 +5,12 @@ import java.util.ArrayList;
 public class Post {
     private String name;
     private String user;
-    private ArrayList<Exercise> exercises;
     private ArrayList<Comment> comments;
     private String description;
     private int likes;
     private boolean isSaved;
 
-    public Post(String name, String user, ArrayList<Exercise> exercises, ArrayList<Comment> comments, String description, int likes) {
-        this.name = name;
-        this.user = user;
-        this.exercises = exercises;
-        this.comments = comments;
-        this.description = description;
-        this.likes = likes;
-        this.isSaved = false;
-    }
+   
 
     public Post(String name, String user, String description, ArrayList<Comment> comments, int likes) {
         this.name = name;
@@ -43,7 +34,6 @@ public class Post {
     public Post(String user) {
         this.user = user;
         this.name = "";
-        this.exercises = new ArrayList<>();
         this.comments = new ArrayList<>();
         this.description = "";
         this.likes = 0;
@@ -53,7 +43,6 @@ public class Post {
     public Post() {
         this.name = "";
         this.user = "";
-        this.exercises = new ArrayList<>();
         this.comments = new ArrayList<>();
         this.description = "";
         this.likes = 0;
@@ -95,25 +84,13 @@ public class Post {
     public void setComments(ArrayList<Comment> comments) {
         this.comments = comments;
     }
-
-    public ArrayList<Exercise> getExercises() {
-        return exercises;
-    }
-
-    public void setExercises(ArrayList<Exercise> exercises) {
-        this.exercises = exercises;
-    }
-
+    
     public int getLikes() {
         return likes;
     }
 
     public void setLikes(int likes) {
         this.likes = likes;
-    }
-
-    public void addExercise(Exercise exercise) {
-        this.exercises.add(exercise);
     }
 
     public void addComment(Comment comment) {
@@ -128,23 +105,5 @@ public class Post {
         return name;
     }
 
-    // FOR TERMINAL TESTING
-
-    public void printPost() {
-        System.out.println("User: " + user);
-        System.out.println("Workout Name: " + name);
-        System.out.println();
-        for(Exercise e : exercises) {
-            System.out.println(e.getName() + ": Sets: " +  e.getSets() + " Reps: " + e.getReps() + " " + e.getAdditionalComment());
-        }
-        System.out.println();
-        System.out.println(description);
-        System.out.println("Likes: " + likes + "                             Comments:");
-
-        if(comments.size() != 0) {
-            for(Comment c : comments) {
-                System.out.println(c.getName() + ": " + c.getComment());
-            }
-        }
-    }
+    
 }
