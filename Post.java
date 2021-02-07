e9e347b19a57b776f7f7f5e50c9e52a57176f4de
ssankha <shiv.sankhavaram@gmail.com>
2021-02-07 03:20:12 -0500

    Added some code when you click on posts

--- Post.java
+++ Post.java
@@ -1,109 +1,37 @@
-package com.company;
+package com.example.exershare;
 
-import java.util.ArrayList;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class Post {
-    private String name;
-    private String user;
-    private ArrayList<Comment> comments;
-    private String description;
-    private int likes;
-    private boolean isSaved;
+import androidx.appcompat.app.AppCompatActivity;
 
-   
+import org.w3c.dom.Text;
 
-    public Post(String name, String user, String description, ArrayList<Comment> comments, int likes) {
-        this.name = name;
-        this.user = user;
-        this.comments = comments;
-        this.description = description;
-        this.likes = likes;
-        this.isSaved = false;
+public class Post extends AppCompatActivity {
+
+    int numLikes = 17;
+    TextView displayLikes;
+    ImageView likeButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+        getSupportActionBar().hide();
     }
 
-    public Post(String name, String user, ArrayList<Comment> comments,
-                String description, int likes, boolean isSaved) {
-        this.name = name;
-        this.user = user;
-        this.comments = comments;
-        this.description = description;
-        this.likes = likes;
-        this.isSaved = isSaved;
+    public void increment(View view) {
+        displayLikes = findViewById(R.id.post_likes);
+        likeButton = findViewById(R.id.like_button);
+
+        numLikes++;
+        displayLikes.setText(Integer.toString(numLikes));
     }
 
-    public Post(String user) {
-        this.user = user;
-        this.name = "";
-        this.comments = new ArrayList<>();
-        this.description = "";
-        this.likes = 0;
-        this.isSaved = false;
-    }
 
-    public Post() {
-        this.name = "";
-        this.user = "";
-        this.comments = new ArrayList<>();
-        this.description = "";
-        this.likes = 0;
-        this.isSaved = false;
-    }
 
-    public String getUser() {
-        return user;
-    }
 
-    public void save() {
-        isSaved = true;
-    }
-
-    public boolean isSaved() {
-        return isSaved;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public ArrayList<Comment> getComments() {
-        return comments;
-    }
-
-    public void setComments(ArrayList<Comment> comments) {
-        this.comments = comments;
-    }
-    
-    public int getLikes() {
-        return likes;
-    }
-
-    public void setLikes(int likes) {
-        this.likes = likes;
-    }
-
-    public void addComment(Comment comment) {
-        this.comments.add(comment);
-    }
-
-    public void like() {
-        this.likes++;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    
 }
